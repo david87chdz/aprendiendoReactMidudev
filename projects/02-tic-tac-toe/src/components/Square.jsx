848ee0dad931ba65a59f5b18669bd1322280cba1
david87chdz <david87chdz@gmail.com>
2024-06-28 17:06:01 +0200

    Add standard lint

--- projects/02-tic-tac-toe/src/components/Square.jsx
+++ projects/02-tic-tac-toe/src/components/Square.jsx
@@ -1,14 +1,13 @@
-export const Square = ({ children, isSelected, updateBoard, index}) => {
-    const className = `square ${isSelected ? 'is-selected' : ''}`;
-  
-    const handleClick = () => {
-      console.log('click en el '+ index );
-      updateBoard(index);
-    }
-    return (
-      <div onClick={handleClick} className={className}>
-        {children}
-      </div>
-    )
+export const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+
+  const handleClick = () => {
+    console.log('click en el ' + index);
+    updateBoard(index);
   }
-  
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
