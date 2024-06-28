ab5d6381289feb137312f8ac6e4e993de9e7bd5a
david87chdz <david87chdz@gmail.com>
2024-06-29 00:26:54 +0200

    Mouse over ends

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { useEffect, useState } from 'react';
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({x: 0, y: 0});
+  //Pointer move
+  useEffect(() => {
+    console.log('Efecto', {enabled});
+    const handleMove = (event) => {
+      const {clientX, clientY} = event;
+      console.log('handleMove', {clientX, clientY})
+      setPosition({x: clientX, y: clientY});
+    }
+    if(enabled) {
+      window.addEventListener('pointermove', handleMove);
+    }
+    
+    //clean up
+    // cuando se desmonte el componente
+    // cuando cambian las dependencias antes de ejecutar el efecto de nuevo
+    return () => {
+      window.removeEventListener('pointermove', handleMove);
+      setPosition({x: 0, y: 0});
+    }
+  }, [enabled])
 
-function App() {
-  const [count, setCount] = useState(0)
+  //Change body className
+  useEffect(() => {
+      document.body.classList.toggle('no-cursor', enabled);
+   return () => {
+      document.body.classList.remove('no-cursor');
+   }
+  }, [enabled])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+        <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}
+      />
+    <button onClick={()=> setEnabled(!enabled)}>
+      {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+    </button>
+    </main>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse/>
+    </main>
   )
 }
 
