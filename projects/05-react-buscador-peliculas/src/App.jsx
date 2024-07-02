08371d442aea209019179331fdc5317f3882a47c
david87chdz <david87chdz@gmail.com>
2024-07-02 23:39:38 +0200

    UseRef and changes before search the movies in the api

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,25 +1,71 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect} from 'react'
 
 import { useMovies } from './hooks/useMovies'
 
 import { Movies } from './components/Movies'
+import './App.css'
 
 const API = 'https://www.omdbapi.com/?i=tt3896198&apikey=1ad26af8&s='
 
+function useSearch(){
+  const [search, updateSearch] = useState('')
+  const [ error, setError ] = useState(null)
+  const isFirstInput = useRef(true)
 
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search == ''
+      return
+    }
+
+    if( search === '') {
+      setError('No se puede buscar una película sin título')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con números')
+      return
+    }
+    
+    if (search.length < 3) {
+      setError('Escribe al menos 3 caracteres')
+      return
+    }
+    
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
 
 function App() {
-
+ 
   const { movies } = useMovies()
   
+  const { search, updateSearch, error } = useSearch() 
+  
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(search)
+  }
+
+  const handleChange= (event) => {
+    updateSearch(event.target.value)
+  }
+ 
+  
+
   return (
     <div className='page'>
       <header>
         <h1>Buscador de películas</h1>
-      <form className='form'>
-        <input type="text" placeholder='Avengers, Star Wars, The Matrix....' />
+      <form className='form' onSubmit={ handleSubmit }>
+        <input onChange={handleChange} value={search} name='query' type="text" placeholder='Avengers, Star Wars, The Matrix....' />
         <button type='submit'>Buscar</button>
       </form>
+      {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies} />
